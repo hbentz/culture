4ec7eda735d40ed88a9c8321f24cb0f335ee3bbe
hbentz <hbentz@ualberta.ca>
2020-07-24 22:13:29 -0700

    Made it so I can tell what object I'm hovering over.

--- Assets/GameMasterMain.cs
+++ Assets/GameMasterMain.cs
@@ -3,14 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class GameMasterMain : MonoBehaviour
 {
     public int PlayerIDTurn = 1;
     public bool PlayerMouseButtonDown = false;
     public int NumberTicks = 0;
-    public GameObject
+    public Text HoverDebug;
 
+    // Not sure if these should be public?
+    public GameObject HoverItem;
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +37,7 @@ public class GameMasterMain : MonoBehaviour
         int.TryParse(_testParse, out _chrissInt);
         Debug.Log(_chrissInt + 5);
 
+        // TryParse returns 0 when it cannot parse
         int.TryParse(_evilInt, out _chrissInt);
         Debug.Log(_chrissInt + 5);
     }
@@ -40,22 +45,36 @@ public class GameMasterMain : MonoBehaviour
     // Keep all updates here for readability
     void Update()
     {
-        /* TODO
-         * Have card area for hand 
-        On mousedown
-        Start raycast from mouse away a camera
-        Get first intersect
-        Move until mouseup
-        determine which cardslot the center is over
-        Clamp to center the card onto that
-        If the center is not over return
-        */
+        // Figure out what the player it pointing at:
+        Ray cursorDirection = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit cursorTarget;
 
-        // Primary click 0, context click 1, middle click 2
-        if (Input.GetMouseButtonDown(0))
+        // Updates cursorTarget at the same time as checking for a collision - how handy!
+        if (Physics.Raycast (cursorDirection, out cursorTarget, 1000))
         {
-            Vector3 UserMousePos = Input.mousePosition;
-            Physics.Raycast.
+            
+            GameObject targetObject = cursorTarget.transform.gameObject;
+            // I intend for all collider components to be attached to the visual mesh
+            // incase this isn't the case in the future, check to make sure it's collided
+            // with a visual instead of an actual object
+            if (targetObject.name == "Visuals")
+            {
+                targetObject = targetObject.transform.parent.gameObject;
+            }
+            HoverDebug.text = targetObject.name;
         }
+
+        /* TODO 
+         * On Mousedown
+         * Drag card in XZ
+         * On Mouseup
+         * If there is a dragged object
+         * Check if that object can be placed there
+         * If if can, figure out where the center is over
+         * and snap it to to there (over nothing goes to next)
+         * Otherwise snap it back to the original position
+         */
+        // Primary click 0, context click 1, middle click 2
+        // Input.GetMouseButtonDown(0)
     }
 }

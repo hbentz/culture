6babb170624d7c4029f391e71cff53e28593c37c
hbentz <hbentz@ualberta.ca>
2020-07-25 15:57:22 -0700

    Add some more comprehensive tags in UIProperties. Also add function to determine if nesting operation is valid.

--- Assets/Scripts/GameMasterMain.cs
+++ Assets/Scripts/GameMasterMain.cs
@@ -5,6 +5,9 @@ using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Numerics;
+using Vector3 = UnityEngine.Vector3;
+using UnityEngine.UIElements;
 
 public class GameMasterMain : MonoBehaviour
 {
@@ -28,6 +31,9 @@ public class GameMasterMain : MonoBehaviour
     // Keep all updates here for readability
     void Update()
     {
+        // Reset the overlay text
+        string DebugOverlayText = "";
+
         // Figure out what the player it pointing at:
         Ray cursorDirection = Camera.main.ScreenPointToRay(Input.mousePosition);
 
@@ -36,36 +42,61 @@ public class GameMasterMain : MonoBehaviour
         {
             // I intend for all collider components to be attached to the visual mesh
             // so ObjectClimber isn't strictly necessary here, it is safer
-            GameObject cusorTarget = ObjectClimber(cursorTargetHit.transform.gameObject);
+            GameObject cursorTarget = ObjectClimber(cursorTargetHit.transform.gameObject);
 
             // Update the UI with info about the hovered element
-            HoverDebug.text = cusorTarget.name;
+            DebugOverlayText = "" + cursorTarget.name + "\n";
+
+            // If the player has clicked on a card
+            // Primary click 0, context click 1, middle click 2
+            if (Input.GetMouseButtonDown(0) & cursorTargetHit.collider.tag == "Card")
+            {
+                // Select that object and set it to drag
+                // DragObject being a reference to cursorTarget does not currently matter
+                // because for as long as the player is dragging the card, the raycast will hit it
+                DragObject = cursorTarget;
+                IsDragging = true;
+                
+                //TODO physically move the card towards the camera and scale it
+            }
         }
 
-        // Primary click 0, context click 1, middle click 2
-        if (Input.GetMouseButtonDown(0))
+        // If the player is click and dragging something
+        if (IsDragging)
         {
-            
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
+            Ray DraggingRay = Camera.main.ScreenPointToRay(new Vector3(DragObject.transform.position,,));
+            GameObject _cardTarget;
 
+            /* TODO: 
+             * Drag object
+             * Check for where the card will snap to (_cardTarget)
+             * Update the UI with that info
+             * 
+             */
+            if (Input.GetMouseButtonUp(0))
+            {
+                SnapTo(DragObject.transform, _cardTarget.transform.position, new Vector3(0.0f, 0.3f, 0.0f));
+                IsDragging = false;
+            }
         }
-        /* TODO 
-         * On Mousedown
-         * Drag card in XZ
-         * On Mouseup
-         * If there is a dragged object
-         * Check if that object can be placed there
-         * If if can, figure out where the center is over
-         * and snap it to to there (over nothing goes to next)
-         * Otherwise snap it back to the original position
-         */
-    }
+        
+            /* TODO 
+             * On Mousedown
+             * Drag card in XZ
+             * On Mouseup
+             * If there is a dragged object
+             * Check if that object can be placed there
+             * If if can, figure out where the center is over
+             * and snap it to to there (over nothing goes to next)
+             * Otherwise snap it back to the original position
+             */
+        HoverDebug.text = DebugOverlayText;
+        }
 
-    void SnapTo(GameObject Source, GameObject Target, float ZOffset = 0.3f)
+    void SnapTo(Transform Source, Vector3 Target, Vector3 Offset)
     {
-
+        // Snaps Source to Target with an offset of Offset
+        Source.position = new Vector3(Target.x + Offset.x, Target.y + Offset.y, Target.z + Offset.z);
     }
 
     GameObject ObjectClimber(GameObject Child)
@@ -76,5 +107,33 @@ public class GameMasterMain : MonoBehaviour
             Child = Child.transform.parent.gameObject;
         }
         return Child;
+    }
+
+    bool VaidNest(GameObject Child, GameObject Parent)
+    {
+        // Checks to see if an attempted nesting procedure is valid
+        // Is this the best way to do this?
+        switch (Parent.tag)
+        {
+            // If the parent is a Cardslot
+            case ("CardSlot"):
+                {
+                    // Check its children for cards
+                    foreach (Transform childTransform in Parent.transform)
+                    {
+                        // If there is already one this is not a valid nes
+                        if (childTransform.gameObject.tag == "Card")
+                        {
+                            return false;
+                        }
+                    }
+                    // If there are no cards it IS a valid nest
+                    return true;
+                }
+            default:
+                {
+                    return false;
+                }
+        }
     }
 }

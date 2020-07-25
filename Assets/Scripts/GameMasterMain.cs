eaa690dde5d9522f882700356e720c34f8a9c709
hbentz <hbentz@ualberta.ca>
2020-07-25 12:40:39 -0700

    Took out teaching code from the Gamemaster Additional naming cleanup.

--- Assets/Scripts/GameMasterMain.cs
+++ Assets/Scripts/GameMasterMain.cs
@@ -19,27 +19,6 @@ public class GameMasterMain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Performance profiler (very serious)
-        DateTime _timeLoaded = DateTime.Now;
-        while((DateTime.Now.Ticks - _timeLoaded.Ticks) < 10000000)
-        {
-            NumberTicks++;
-        }
-        Debug.Log(NumberTicks);
-
-        string _testParse = "5";
-        string _evilInt = "3246sasdf874";
-        int _chrissInt = 0;
-
-        // TryParse never throws execption
-        // "out" actually modifies the object directly as if it
-        // were a public var ONLY for that function
-        int.TryParse(_testParse, out _chrissInt);
-        Debug.Log(_chrissInt + 5);
-
-        // TryParse returns 0 when it cannot parse
-        int.TryParse(_evilInt, out _chrissInt);
-        Debug.Log(_chrissInt + 5);
     }
 
     // Keep all updates here for readability

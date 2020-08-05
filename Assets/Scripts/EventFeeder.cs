31c87bf160c2266e526b247eec68ae299107305c
hbentz <hbentz@ualberta.ca>
2020-08-04 21:56:21 -0700

    Added required logic to card prefab and visuals to interact with EventSystem

---
+++ Assets/Scripts/EventFeeder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventFeeder : MonoBehaviour
+{
+    // Intended to handle EventSystem input only
+    void OnMouseDrag()
+    {
+        GameMasterMain.Instance.GenericDrag(this.transform.gameObject);
+    }
+}

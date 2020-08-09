099427eb99f9ead6b2dd1eb5f7f1e1bcb5dff7fa
hbentz <hbentz@ualberta.ca>
2020-08-09 16:32:23 -0700

    Added PlayerBehaviours with listener on OnTurnStart() to make sure the active player has their camerea on.

---
+++ Assets/Scripts/Behaviours/PlayerBehaviours.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviours : MonoBehaviour
+{
+    // This player's camera's GameObject
+    public GameObject CameraHolder;
+
+    private void OnEnable()
+    {
+        // Toggle the camerea based on the active player whenever there is a turn change
+        GameMasterMain.OnTurnStart += UpdateCameraState;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        // Remove listeners to avoid errors
+        GameMasterMain.OnTurnStart -= UpdateCameraState;
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void UpdateCameraState(GameObject ActivePlayer)
+    {
+        // If this is the active player, enable the camera, otherwise disable it
+        CameraHolder.GetComponent<Camera>().enabled = ActivePlayer == this;
+    }
+}

2dd7b2dac588fc202ddc0e4045a7290752b6bf60
hbentz <hbentz@ualberta.ca>
2020-08-09 16:31:30 -0700

    Added CommonBoardBehaivours with listener for OnTurnStart so the board will rotate to face the active player.

---
+++ Assets/Scripts/Behaviours/CommonBoardBehaviours.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonBoardBehaviours : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        // Whenever a turn starts set the board to face the player
+        GameMasterMain.OnTurnStart += RotateBoardtoActivePlayer;
+    }
+    void Start()
+    {
+    }
+
+    private void OnDisable()
+    {
+        // Remove SetBoardFacePlayer as a listener to the OnTurnStart method to avoid errors
+        GameMasterMain.OnTurnStart -= RotateBoardtoActivePlayer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+    }
+
+    public void RotateBoardtoActivePlayer(GameObject _activePlayer)
+    {
+        // Gets the active player's rotation and sets the baord to face them
+        this.transform.rotation = _activePlayer.transform.rotation;
+    }
+}

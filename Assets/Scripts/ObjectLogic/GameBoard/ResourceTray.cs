99e034a161da9956af2597a48c987f539a2a6668
hbentz <hbentz@ualberta.ca>
2020-09-07 00:09:50 -0700

    ResrouceTrays and Cards now how lists of hosted objects on them.

--- Assets/Scripts/ObjectLogic/GameBoard/ResourceTray.cs
+++ Assets/Scripts/ObjectLogic/GameBoard/ResourceTray.cs
@@ -1,3 +1,20 @@
-﻿public class ResourceTray : GameBoard
+﻿using System.Collections.Generic;
+
+public class ResourceTray : GameBoard
 {
+	public int MaximumTotalCurrency;
+	public List<Currency> InvestorCredits;
+	public List<Currency> IntellectualPropertys;
+	public List<Currency> PublicRelations;
+	public List<Currency> EmployeeMotivations;
+	public List<Currency> Managers;
+	public List<Currency> Teams;
+
+	public int CurrentTotalCurrency {
+		get
+		{
+			// Add up all the items in all the lengths of the monetary currency lists to get this number
+			return InvestorCredits.Count + IntellectualPropertys.Count + PublicRelations.Count + EmployeeMotivations.Count;
+		}
+	}
 }
--- Assets/Scripts/ObjectLogic/GameResource/Card.cs
+++ Assets/Scripts/ObjectLogic/GameResource/Card.cs
@@ -3,4 +3,13 @@ using UnityEngine;
 
 public abstract class Card : GameResource
 {
+    public CardType EnumCardType;
+	public List<CurrencyType> HostableResources;
+
+	public List<Currency> InvestorCredits;
+	public List<Currency> IntellectualPropertys;
+	public List<Currency> PublicRelations;
+	public List<Currency> EmployeeMotivations;
+	public List<Currency> Managers;
+	public List<Currency> Teams;
 }

09b5de4a9ef85353f3638fad39bb874c885a9f68
hbentz <hbentz@ualberta.ca>
2020-09-07 14:33:55 -0700

    Added turn/phase/round end/start events to GameMaster

--- Assets/Scripts/GameLogic/GameMaster.cs
+++ Assets/Scripts/GameLogic/GameMaster.cs
@@ -7,6 +7,28 @@ public class GameMaster : MonoBehaviour
     private static GameMaster instance = null;
     public static GameMaster Instance { get { return instance; } }
 
+    // GameState Stuff
+    public TurnInfo TurnState;
+
+    // Turn Events in order in which they will occur
+    public delegate void RoundStart();
+    public static event RoundStart OnRoundStarted;
+
+    public delegate void PhaseStart();
+    public static event PhaseStart OnPhaseStarted;
+
+    public delegate void TurnStart();
+    public static event TurnStart OnTurnStarted;
+
+    public delegate void TurnEnd();
+    public static event TurnEnd OnTurnEnded;
+
+    public delegate void PhaseEnd();
+    public static event PhaseEnd OnPhaseEnded;
+
+    public delegate void RoundEnd();
+    public static event RoundEnd OnRoundEnded;
+
     private void Awake()
     {
         // Sets the instance of GameMasterMain to this one on game start

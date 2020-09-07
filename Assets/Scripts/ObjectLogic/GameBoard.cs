a2ce6f98839e0626e9d53aa4681f1fddbaa6f73d
hbentz <hbentz@ualberta.ca>
2020-09-07 00:09:22 -0700

    GameBoards now also have a referenceable enum for easy coding

--- Assets/Scripts/ObjectLogic/GameBoard.cs
+++ Assets/Scripts/ObjectLogic/GameBoard.cs
@@ -6,5 +6,6 @@ public abstract class GameBoard : MonoBehaviour
 {
     public Dictionary<BoardType, List<GameBoard>> NestedBoards;
     public Dictionary<CardType, Dictionary<DeckType, List<Deck>>> CardHosts;
+    public BoardType BoardEnumType;
 }
 

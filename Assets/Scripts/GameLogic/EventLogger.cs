d26780caef614304ec310ffc5db0c20b22ac6dac
hbentz <hbentz@ualberta.ca>
2020-09-07 17:24:14 -0700

    Simplified references in EventLogger to TurnInfo

--- Assets/Scripts/GameLogic/EventLogger.cs
+++ Assets/Scripts/GameLogic/EventLogger.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EventLogger : MonoBehaviour
 {
+
+    public TurnInfo TurnInfo = TurnInfo.Instance;
+
     private void OnEnable()
     {
         GameMaster.OnRoundStarted += RoundStart;
@@ -26,32 +29,32 @@ public class EventLogger : MonoBehaviour
 
     private void RoundStart()
     {
-        Debug.Log($"OnRoundStarted event fired begining Round {TurnInfo.Instance.CurrentRound}.");
+        Debug.Log($"OnRoundStarted event fired begining round {TurnInfo.CurrentRound}.");
     }
 
     private void PhaseStart()
     {
-        Debug.Log($"OnPhaseStarted event fired begining Phase {TurnInfo.Instance.CurrentPhase}.");
+        Debug.Log($"OnPhaseStarted event fired begining phase: {TurnInfo.CurrentPhase}.");
     }
 
     private void TurnStart()
     {
-        Debug.Log($"OnTurnStarted event fired begining the turn for {TurnInfo.Instance.ActivePlayer.PlayerName}.");
-        Debug.Log($"This is turn {TurnInfo.Instance.TurnCounter} in the phase.");
+        Debug.Log($"OnTurnStarted event fired begining the turn for {TurnInfo.ActivePlayer.PlayerName}.");
+        Debug.Log($"This is turn {TurnInfo.TurnCounter} in the phase.");
     }
 
     private void TurnEnd()
     {
-        Debug.Log($"OnTurnEnded event fired ending the turn for {TurnInfo.Instance.ActivePlayer.PlayerName}.");
+        Debug.Log($"OnTurnEnded event fired ending the turn for {TurnInfo.ActivePlayer.PlayerName}.");
     }
 
     private void PhaseEnd()
     {
-        Debug.Log($"OnPhaseEnded event fired ending the phase {TurnInfo.Instance.CurrentPhase}.");
+        Debug.Log($"OnPhaseEnded event fired ending the phase: {TurnInfo.CurrentPhase}.");
     }
 
     private void RoundEnd()
     {
-        Debug.Log($"OnRoundEnded event fired ending round {TurnInfo.Instance.CurrentRound}.");
+        Debug.Log($"OnRoundEnded event fired ending round {TurnInfo.CurrentRound}.");
     }
 }

ed2a9e6a026f8f364f254c0053f8fa5aace3b147
hbentz <hbentz@ualberta.ca>
2020-09-07 22:25:21 -0700

    First attempt at card factory

---
+++ Assets/CardFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class CardFactory : MonoBehaviour
+{
+    public GameObject GenericCardPrefab;
+    
+    public Card MakeCard<Card>()
+    {
+        // Make a new prefab instance
+        GameObject _newCardObject = Instantiate(GenericCardPrefab);
+        Type asdf = typeof(card);   
+        // Give it a card and get the reference too it
+        _newCardObject.AddComponent<Card>();
+        Card _newCard = _newCardObject.GetComponent<typeOfCard>();
+
+        // Set the 
+    }
+}

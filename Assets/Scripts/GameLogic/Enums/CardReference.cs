932e86fe79ea3ed4f3d60059f675c2c65428b617
hbentz <hbentz@ualberta.ca>
2020-09-09 00:09:54 -0700

    Got the basics of the card factory going.

---
+++ Assets/Scripts/GameLogic/Enums/CardReference.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System;
+
+public static class CardReference
+{
+    public enum CardName
+    {
+        TestDoNothing,
+    }
+
+    public static Dictionary<CardName, Type> CardLookup = new Dictionary<CardName, Type>()
+    {
+        {CardName.TestDoNothing, typeof(TestDoNothing)}
+    };
+}

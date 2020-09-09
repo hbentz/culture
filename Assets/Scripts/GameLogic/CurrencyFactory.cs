af0f2f997d5f29b0e76298425cc4499580f15ba9
hbentz <hbentz@ualberta.ca>
2020-09-09 01:03:46 -0700

    Uses prefabs and a CurrencyType enum to make a currency of the appropriate type

---
+++ Assets/Scripts/GameLogic/CurrencyFactory.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+public class CurrencyFactory : MonoBehaviour
+{
+    public static CurrencyFactory Instance;
+
+    public GameObject CurrencyPrefab;
+    public GameObject ManagerPrefab;
+    public GameObject TeamPrefab;
+
+	public Currency MakeCurrency(CurrencyType _type)
+    {
+        GameObject _newObj;
+        Currency _returnCurrency;
+
+        switch (_type)
+        {
+            case CurrencyType.InvestorCredit:
+                _newObj = Instantiate(CurrencyPrefab);
+                _returnCurrency = _newObj.AddComponent<InvestorCredit>();
+                break;
+
+            case CurrencyType.IntellectualProperty:
+                _newObj = Instantiate(CurrencyPrefab);
+                _returnCurrency = _newObj.AddComponent<IntellectualProperty>();
+                break;
+
+            case CurrencyType.PublicRelation:
+                _newObj = Instantiate(CurrencyPrefab);
+                _returnCurrency = _newObj.AddComponent<PublicRelation>();
+                break;
+
+            case CurrencyType.EmployeeMotivation:
+                _newObj = Instantiate(CurrencyPrefab);
+                _returnCurrency = _newObj.AddComponent<EmployeeMotivation>();
+                break;
+
+            case CurrencyType.WorkerManager:
+                _newObj = Instantiate(ManagerPrefab);
+                _returnCurrency = _newObj.AddComponent<WorkerManager>();
+                break;
+
+            case CurrencyType.WorkerTeam:
+                _newObj = Instantiate(TeamPrefab);
+                _returnCurrency = _newObj.AddComponent<WorkerTeam>();
+                break;
+
+            default:
+                _newObj = Instantiate(CurrencyPrefab);
+                _returnCurrency = _newObj.AddComponent<Currency>();
+                Debug.LogError("Tried to give player untyped currency");
+                break;
+        }
+
+        return _returnCurrency;
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+}

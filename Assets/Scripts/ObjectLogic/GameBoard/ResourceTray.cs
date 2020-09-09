ca9e0e7fcb0adf5b468218fe6ce4b636689b68d6
hbentz <hbentz@ualberta.ca>
2020-09-09 00:52:30 -0700

    Initialize the list and dict of hosted currency on the resource tray on wake.

--- Assets/Scripts/ObjectLogic/GameBoard/ResourceTray.cs
+++ Assets/Scripts/ObjectLogic/GameBoard/ResourceTray.cs
@@ -3,12 +3,14 @@
 public class ResourceTray : GameBoard
 {
 	public int MaximumTotalCurrency;
-	public List<Currency> InvestorCredits;
-	public List<Currency> IntellectualPropertys;
-	public List<Currency> PublicRelations;
-	public List<Currency> EmployeeMotivations;
-	public List<Currency> Managers;
-	public List<Currency> Teams;
+	public List<Currency> InvestorCredits = new List<Currency>();
+	public List<Currency> IntellectualPropertys = new List<Currency>();
+	public List<Currency> PublicRelations = new List<Currency>();
+	public List<Currency> EmployeeMotivations = new List<Currency>();
+	public List<Currency> WorkerManagers = new List<Currency>();
+	public List<Currency> WorkerTeams = new List<Currency>();
+
+	public Dictionary<CurrencyType, List<Currency>> HostedCurrency;
 
 	public int CurrentTotalCurrency {
 		get
@@ -16,5 +18,18 @@ public class ResourceTray : GameBoard
 			// Add up all the items in all the lengths of the monetary currency lists to get this number
 			return InvestorCredits.Count + IntellectualPropertys.Count + PublicRelations.Count + EmployeeMotivations.Count;
 		}
+	}
+
+    private void Awake()
+    {
+		HostedCurrency = new Dictionary<CurrencyType, List<Currency>>()
+		{
+			{ CurrencyType.InvestorCredit, InvestorCredits },
+			{ CurrencyType.IntellectualProperty, IntellectualPropertys },
+			{ CurrencyType.PublicRelation, PublicRelations },
+			{ CurrencyType.EmployeeMotivation, EmployeeMotivations },
+			{ CurrencyType.WorkerManager, WorkerManagers },
+			{ CurrencyType.WorkerTeam, WorkerTeams },
+		};
 	}
 }

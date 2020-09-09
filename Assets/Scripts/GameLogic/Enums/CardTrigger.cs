4128f661f1f0b5d96951783b9ec96f923b44b4a1
hbentz <hbentz@ualberta.ca>
2020-09-09 00:10:09 -0700

    Added CardTrigger enum

---
+++ Assets/Scripts/GameLogic/Enums/CardTrigger.cs
@@ -0,0 +1,17 @@
+﻿public enum CardTrigger
+{
+    Play,
+    Destroy,
+    Activate,
+    Manage,
+    StopManage,
+    Develop,
+    Disable,
+
+    RoundStart,
+    PhaseStart,
+    TurnStart,
+    TurnEnd,
+    PhaseEnd,
+    RoundEnd,
+}
